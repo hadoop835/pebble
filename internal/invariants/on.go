8b6a9571dd31a2cfa6482e71ec0332b23772c4db
Radu Berinde <radu@cockroachlabs.com>
2024-10-21 12:32:00 -0700

    turn on invariants all the time (test only)

--- internal/invariants/on.go
+++ internal/invariants/on.go
@@ -2,9 +2,6 @@
 // of this source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
 
-//go:build invariants || race
-// +build invariants race
-
 package invariants
 
 // Enabled is true if we were built with the "invariants" or "race" build tags.
